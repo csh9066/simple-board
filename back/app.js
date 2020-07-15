29325de6085e4847a1534991fe7599254ca439f1
seungho <50465901+csh9066@users.noreply.github.com>
2020-07-15 21:42:10 +0900

    cors 미들웨어 추가

--- back/app.js
+++ back/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 const memberRouter = require('./routes/memberRouter');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
