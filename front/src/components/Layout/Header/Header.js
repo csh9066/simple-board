ee5890d9c818d7028910b571e13c2068d947e8e1
seungho <50465901+csh9066@users.noreply.github.com>
2020-07-18 22:03:49 +0900

    헤더 컴포넌트 로그인에 유무에 따라 렌더링 상태 변경

--- front/src/components/Layout/Header/Header.js
+++ front/src/components/Layout/Header/Header.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { Layout, Divider } from 'antd';
+import React, { useContext } from 'react';
+import { Layout, Divider, Button } from 'antd';
 import { Link } from 'react-router-dom';
 
 import './Header.css';
+import MeContext from '../../../context/MeContext';
+import { requestLogout } from '../../../api/member';
 
 const Header = () => {
+	const { me, setMe } = useContext(MeContext);
+
+	const onClickLogout = async () => {
+		await requestLogout();
+		setMe(null);
+	};
+
 	return (
 		<Layout.Header className="header">
 			<div className="logo">
 				<Link to="/">Simple Board</Link>
 			</div>
 			<div className="header-menu">
-				<Link to="/login">로그인</Link>
-				<Divider type="vertical" />
-				<Link to="/signup">회원 가입</Link>
+				{me ? (
+					<>
+						{me.nickname}님 환영합니다
+						<Button onClick={onClickLogout}>로그 아웃</Button>
+					</>
+				) : (
+					<>
+						<Link to="/login">로그인</Link>
+						<Divider type="vertical" />
+						<Link to="/signup">회원 가입</Link>
+					</>
+				)}
 			</div>
 		</Layout.Header>
 	);
