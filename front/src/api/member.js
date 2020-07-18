4fee440ed609ced43aab0676b8c08193f3d81e3e
seungho <50465901+csh9066@users.noreply.github.com>
2020-07-18 21:58:46 +0900

    프론트 로그인, 로그아웃 api 생성

---
+++ front/src/api/member.js
@@ -0,0 +1,16 @@
+import Axios from 'axios';
+
+const axios = Axios.create({
+	baseURL: 'http://localhost:3065/member/',
+	withCredentials: true,
+});
+
+export const requestLogin = async (data) => {
+	const resopnse = await axios.post('/login', data);
+	return resopnse.data;
+};
+
+export const requestLogout = async () => {
+	const response = await axios.post('/logout');
+	return response.data;
+};
