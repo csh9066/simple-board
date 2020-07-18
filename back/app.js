2c05e8a39c1f97aa8302fa1c8a7a34b85e8c62b5
seungho <50465901+csh9066@users.noreply.github.com>
2020-07-18 19:20:38 +0900

    app.js cors,session 미들웨어 추가

--- back/app.js
+++ back/app.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const session = require('express-session');
 
 const app = express();
 
 const memberRouter = require('./routes/memberRouter');
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+	})
+);
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(
+	session({
+		secret: 'secret',
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			httpOnly: false,
+		},
+	})
+);
 
 app.get('/', (req, res) => {
 	res.send('first');
